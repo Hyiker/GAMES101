390ca28304a7aa087b9eb9c81af26039c32939bf
Hyiker <961531608@qq.com>
2021-08-30 19:48:00 +0800

    fill in interpolation in Assignment3

--- Assignment3/rasterizer.hpp
+++ Assignment3/rasterizer.hpp
@@ -5,9 +5,12 @@
 #pragma once
 
 #include <eigen3/Eigen/Eigen>
+#include <optional>
 #include <algorithm>
 #include "global.hpp"
+#include "Shader.hpp"
 #include "Triangle.hpp"
+
 using namespace Eigen;
 
 namespace rst
@@ -61,23 +64,30 @@ namespace rst
         pos_buf_id load_positions(const std::vector<Eigen::Vector3f>& positions);
         ind_buf_id load_indices(const std::vector<Eigen::Vector3i>& indices);
         col_buf_id load_colors(const std::vector<Eigen::Vector3f>& colors);
+        col_buf_id load_normals(const std::vector<Eigen::Vector3f>& normals);
 
         void set_model(const Eigen::Matrix4f& m);
         void set_view(const Eigen::Matrix4f& v);
         void set_projection(const Eigen::Matrix4f& p);
 
-        void set_pixel(const Eigen::Vector3f& point, const Eigen::Vector3f& color);
+        void set_texture(Texture tex) { texture = tex; }
+
+        void set_vertex_shader(std::function<Eigen::Vector3f(vertex_shader_payload)> vert_shader);
+        void set_fragment_shader(std::function<Eigen::Vector3f(fragment_shader_payload)> frag_shader);
+
+        void set_pixel(const Vector2i &point, const Eigen::Vector3f &color);
 
         void clear(Buffers buff);
 
         void draw(pos_buf_id pos_buffer, ind_buf_id ind_buffer, col_buf_id col_buffer, Primitive type);
+        void draw(std::vector<Triangle *> &TriangleList);
 
         std::vector<Eigen::Vector3f>& frame_buffer() { return frame_buf; }
 
     private:
         void draw_line(Eigen::Vector3f begin, Eigen::Vector3f end);
 
-        void rasterize_triangle(const Triangle& t);
+        void rasterize_triangle(const Triangle& t, const std::array<Eigen::Vector3f, 3>& world_pos);
 
         // VERTEX SHADER -> MVP -> Clipping -> /.W -> VIEWPORT -> DRAWLINE/DRAWTRI -> FRAGSHADER
 
@@ -86,16 +96,21 @@ namespace rst
         Eigen::Matrix4f view;
         Eigen::Matrix4f projection;
 
+        int normal_id = -1;
+
         std::map<int, std::vector<Eigen::Vector3f>> pos_buf;
         std::map<int, std::vector<Eigen::Vector3i>> ind_buf;
         std::map<int, std::vector<Eigen::Vector3f>> col_buf;
+        std::map<int, std::vector<Eigen::Vector3f>> nor_buf;
+
+        std::optional<Texture> texture;
+
+        std::function<Eigen::Vector3f(fragment_shader_payload)> fragment_shader;
+        std::function<Eigen::Vector3f(vertex_shader_payload)> vertex_shader;
 
         std::vector<Eigen::Vector3f> frame_buf;
-
-        // we **must** use a color buffer instead of directly calc
-        // the average of the sum to avoid black border
-        std::vector<std::array<Eigen::Vector3f, 4>> color_buf;
         std::vector<std::array<float, 4>> depth_buf;
+        std::vector<std::array<Eigen::Vector3f, 4>> color_buf;
         int get_index(int x, int y);
 
         int width, height;
